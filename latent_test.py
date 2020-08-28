9ec17ce18950de8eca9707f08406b1dce98f59a8
ZeroAct <46982469+ZeroAct@users.noreply.github.com>
2020-08-28 14:00:11 +0900

    Update latent_test.py

--- latent_test.py
+++ latent_test.py
@@ -24,4 +24,29 @@ x_valid = x_valid / 127.5 - 1
 
 auto_encoder = tf.keras.models.load_model('./denoise_latent512/final.h5')
 
-encoder = Model(Input(shape=(32,32,3)), auto_encoder.layers[1].outputs[0])
+encoder = auto_encoder.layers[1]
+decoder = auto_encoder.layers[2]
+
+test_img = x_train[7].copy()
+noise_img = test_img + 0.1*np.random.randn(32,32,3)
+
+latent = encoder.predict(np.array([noise_img]))
+result = decoder.predict(latent)
+
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(1, 2)
+fig.set_size_inches(12, 6)
+
+t = test_img
+t = (t-t.min())/(t.max()-t.min())*255
+axes[0].imshow(t.astype(np.uint8))
+axes[0].axis('off')
+
+t = result[0]
+t = (t-t.min())/(t.max()-t.min())*255
+axes[1].imshow(t.astype(np.uint8))
+axes[1].axis('off')
+
+plt.tight_layout()
+plt.show()
